bd8347a93cb704a4497092a33ef1e871d55a17f2
Andrés David Montoya Aguirre <admont28@gmail.com>
2016-07-08 21:50:55 -0500

    Mejora del menú.
    * Se mejora el menú presentando las acciones administrativas en él,
    para que el usuario tenga un mejor acceso.

--- application/views/plantilla/nav.php
+++ application/views/plantilla/nav.php
@@ -9,42 +9,88 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <div id="menu">
-	  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
 	    <!-- Brand and toggle get grouped for better mobile display -->
 	    <div class="navbar-header">
-	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex6-collapse">
-	        <span class="sr-only">Desplegar navegación</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	      <a class="navbar-brand" href="<?php echo base_url(); ?>">Tratamiento de Heridas</a>
+	      	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex6-collapse">
+	        	<span class="sr-only">Desplegar navegación</span>
+	        	<span class="icon-bar"></span>
+	        	<span class="icon-bar"></span>
+	        	<span class="icon-bar"></span>
+	      	</button>
+	      	<a class="navbar-brand" href="<?php echo base_url(); ?>">Tratamiento de Heridas</a>
 	    </div>
 
 	    <!-- Collect the nav links, forms, and other content for toggling -->
 	    <div class="collapse navbar-collapse navbar-ex6-collapse">
-	      <ul class="nav navbar-nav">
-	        <li class="<?php if(isset($controller) && $controller == 'Inicio') echo 'active'; ?>"><a href="<?php echo base_url(); ?>">Inicio</a></li>
-	        <li class="<?php if(isset($controller) && $controller == 'SituacionEnfermeria') echo 'active'; ?>"><a href="<?php echo base_url('SituacionEnfermeria') ?>">Situacion de Enfermeria</a></li>
-	      </ul>
-	    <ul class="nav navbar-nav navbar-right" style="margin-right: 0 !important;">
-			<?php if($this->session->has_userdata('usuario')): ?>
-		      		<li>
+	      	<ul class="nav navbar-nav">
+	        	<li class="<?php if(isset($controller) && $controller == 'Inicio') echo 'active'; ?>"><a href="<?php echo base_url(); ?>">Inicio</a></li>
+	        	<li class="<?php if(isset($controller) && $controller == 'SituacionEnfermeria') echo 'active'; ?>"><a href="	<?php echo base_url('SituacionEnfermeria') ?>">Situacion de Enfermeria</a></li>
+	      	</ul>
+	    	<ul class="nav navbar-nav navbar-right" style="margin-right: 0 !important;">
+		    	<?php $session = $this->session->has_userdata('usuario'); ?>
+		    	<?php if($session): ?>
+		    		<?php $identificacion_usuario = $this->session->usuario['identificacion_usuario']; ?>
+		    		<?php $rol_usuario = $this->session->usuario['rol_usuario']; ?>
+		    		<?php if ($rol_usuario == "admin"): ?>
+		    			<li class="dropdown">
+				          	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Administración <span class="caret"></span></a>
+				          	<ul class="dropdown-menu">
+				          		<li class="dropdown-header">Administración de tipos de herida</li>
+				            	<li>
+				            		<a href="<?php echo base_url('Administrador/administracion-de-tipos-de-heridas'); ?>">Visualizar los tipos de herida</a>
+				            	</li>
+				            	<li>
+				            		<a href="<?php echo base_url('Administrador/formulario-adicionar-tipo-de-herida'); ?>">Adicionar tipo de herida</a>
+				            	</li>
+				            	<li role="separator" class="divider"></li>
+				            	<li class="dropdown-header">Administración de factores de riesgo</li>
+				            	<li>
+				            		<a href="<?php echo base_url('Administrador/administracion-de-factores-de-riesgo'); ?>">Visualizar los factores de riesgo</a>
+				            	</li>
+				            	<li>
+				            		<a href="<?php echo base_url('Administrador/formulario-adicionar-factor-de-riesgo'); ?>">Adicionar factor de riesgo</a>
+				            	</li>
+				            	<li role="separator" class="divider"></li>
+				            	<li class="dropdown-header">Administración de actividades</li>
+				            	<li>
+				            		<a href="<?php echo base_url('Administrador/administracion-de-actividades'); ?>">Visualizar las actividades</a>
+				            	</li>
+				            	<li>
+				            		<a href="<?php echo base_url('Administrador/formulario-adicionar-actividad'); ?>">Adicionar actividad</a>
+				            	</li>
+				            	<li role="separator" class="divider"></li>
+				            	<li class="dropdown-header">Administración de usuarios</li>
+				            	<li>
+				            		<a href="<?php echo base_url('Administrador/administracion-de-usuarios'); ?>">Visualizar los usuarios</a>
+				            	</li>
+				          </ul>
+				        </li>
+		    		<?php endif; ?>
+		    		<li class="dropdown">
+				          	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pacientes <span class="caret"></span></a>
+				          	<ul class="dropdown-menu">
+				            	<li>
+				            		<a href="<?php echo base_url('Usuario/formulario-adicionar-paciente'); ?>">Adicionar paciente</a>
+				            	</li>
+				          </ul>
+				        </li>
+		    		<li>
 		      			<a href="<?php echo base_url('Usuario/perfil'); ?>">
-		      				<span class="glyphicon glyphicon-user" aria-hidden="true"></span> Usuario: <?php echo $this->session->usuario['identificacion_usuario'] ?>
+		      				<span class="glyphicon glyphicon-user" aria-hidden="true"></span> Usuario: <?php echo $identificacion_usuario ?>
 		      			</a>
 		      		</li>
 		      		<li>
 		      			<a href="<?php echo base_url('Usuario/cerrar-sesion'); ?>">Cerar sesión
 		      			</a>
 		      		</li>
-			<?php else: ?>
+		      	<?php else: ?>
 					<li><a href="<?php echo base_url('Usuario/formulario-de-registro-de-usuario'); ?>"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Registrarme</a></li>
 		      		<li><a href="<?php echo base_url('Usuario/formulario-inicio-de-sesion'); ?>">Iniciar Sesión</a></li>
-			<?php endif; ?>
-		    </ul>
+		    	<?php endif; ?>
+			</ul>
 	    </div><!-- /.navbar-collapse -->
-	  </nav>
+	</nav>
 </div>
 <div style="margin-top: 50px;">
 	<?php echo $this->breadcrumb->output(); ?>
@@ -65,6 +111,5 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 					</div>
 				<?php endif; ?>
 			<?php endif; ?>
-			
 		<?php endforeach; ?>
 	<?php endif; ?>
